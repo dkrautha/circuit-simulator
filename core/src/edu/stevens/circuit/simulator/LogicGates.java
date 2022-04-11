a6a26c2da09cec8b615f2dffaba5ef4f8535b028
David Krauthamer <david_krauthamer@protonmail.com>
2022-04-11 18:57:12 -0400

    Add libGDX Hello World

---
+++ core/src/edu/stevens/circuit/simulator/LogicGates.java
@@ -0,0 +1,17 @@
+package edu.stevens.circuit.simulator;
+
+class LogicGates {
+  public boolean not(boolean a) { return !a; }
+
+  public boolean and(boolean a, boolean b) { return true; }
+
+  public boolean or(boolean a, boolean b) { return true; }
+
+  public boolean nand(boolean a, boolean b) { return true; }
+
+  public boolean nor(boolean a, boolean b) { return true; }
+
+  public boolean xor(boolean a, boolean b) { return true; }
+
+  public boolean xnor(boolean a, boolean b) { return true; }
+}
