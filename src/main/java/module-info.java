8761bb55a39cda3b67992617961543bcf9905b56
David Krauthamer <david_krauthamer@protonmail.com>
2022-04-25 20:48:42 -0400

    Add graph library and update to java 17

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module edu.stevens.circuit.simulator {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.jgrapht.core;
     requires transitive javafx.graphics;
 
     opens edu.stevens.circuit.simulator to javafx.fxml;
