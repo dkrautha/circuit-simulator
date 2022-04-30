ad9457281483c60a14cf3c1c441e2027b43082ad
David Krauthamer <david_krauthamer@protonmail.com>
2022-04-29 23:11:01 -0400

    Add GateNot class

---
+++ src/main/java/edu/stevens/circuit/simulator/GateNot.java
@@ -0,0 +1,23 @@
+package edu.stevens.circuit.simulator;
+
+import java.util.Arrays;
+
+public class GateNot extends Gate {
+    public GateNot(Wire input, Wire out) {
+        super("NOT", Arrays.asList(input), out);
+    }
+
+    @Override
+    public boolean propagate() {
+        Wire outWire = getOutput();
+        Signal oldSignal = outWire.getSignal();
+        Signal newSignal = getInputs().get(0).getSignal().invert();
+
+        if (oldSignal.equals(newSignal)) {
+            return false;
+        }
+
+        outWire.setSignal(newSignal);
+        return true;
+    }
+}
