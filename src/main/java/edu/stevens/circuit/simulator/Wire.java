8ae15ded930ba5438d6041ba4592bf19ec54d88c
David Krauthamer <david_krauthamer@protonmail.com>
2022-05-08 15:31:38 -0400

    Update auto-generated equals and hashcode for Wire

--- src/main/java/edu/stevens/circuit/simulator/Wire.java
+++ src/main/java/edu/stevens/circuit/simulator/Wire.java
@@ -1,5 +1,7 @@
 package edu.stevens.circuit.simulator;
 
+import java.util.Objects;
+
 /**
  * Represents the connectors between gates and circuits.
  * 
@@ -21,30 +23,19 @@ public class Wire {
 
     @Override
     public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((name == null) ? 0 : name.hashCode());
-        result = prime * result + ((signal == null) ? 0 : signal.hashCode());
-        return result;
+        return Objects.hash(name, signal);
     }
 
     @Override
     public boolean equals(Object obj) {
-        if (this == obj)
+        if (this == obj) {
             return true;
-        if (obj == null)
+        }
+        if (!(obj instanceof Wire)) {
             return false;
-        if (getClass() != obj.getClass())
-            return false;
+        }
         Wire other = (Wire) obj;
-        if (name == null) {
-            if (other.name != null)
-                return false;
-        } else if (!name.equals(other.name))
-            return false;
-        if (signal != other.signal)
-            return false;
-        return true;
+        return Objects.equals(name, other.name) && signal == other.signal;
     }
 
     public Signal getSignal() {
